071aef4ef575aa2a3e8d72826a424dc13c409d52
0wsmith <01wsmith@gmail.com>
2017-11-12 20:40:02 +0000

    Output
    The output of the current version of the converter.

---
+++ Java-to-JavaScript/JavaProgram.js
@@ -0,0 +1,19 @@
+
+class JavaProgram {
+
+	var wrong = 1;
+	var title;
+
+	function main(var args[]) {
+		System.out.println("Hello World!");
+	}
+
+	var getWrong() {
+		return JavaProgram.wrong;
+	}
+
+	function setWrong(var newValue) {
+		JavaProgram.wrong = newValue;
+	}
+
+}
